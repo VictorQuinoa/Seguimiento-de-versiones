538fa08e1988e71011cfad1aa343894c8ef8f49e
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-10-30 09:52:06 +0100

    06_3

---
+++ NetBeansProjects/Boletin06_3/src/main/java/com/mycompany/boletin06_3/Boletin06_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin06_3;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin06_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/Boletin06_3/src/main/java/com/mycompany/boletin06_3/Circulo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.boletin06_3;
+
+/**
+ *
+ * @author dam1
+ */
+public class Circulo{
+    private double radio;
+    private double pi ;
+}
+//constructor por defecto
+public Circulo(){
+
+}
+
+public Circulo(double radio, double pi)}{
+radio= r;
+pi = p;
+}
+ 
+
+
+}
