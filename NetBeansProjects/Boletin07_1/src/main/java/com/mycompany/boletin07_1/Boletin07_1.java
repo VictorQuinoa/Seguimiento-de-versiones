8e510dcccc80a276c3274bf53f56dd6519cfa95b
VictorQuinoa <vquinoamunoz@danielcastelao.org>
2023-10-30 09:53:33 +0100

    07_1

---
+++ NetBeansProjects/Boletin07_1/src/main/java/com/mycompany/boletin07_1/Boletin07_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletin07_1;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin07_1 {
+    
+public static void main(String[] args) {
+    
+   NumeroPositivo obx =new NumeroPositivo();
+   obx.mostrarPositivo(2);
+         
+    }
+}
---
+++ NetBeansProjects/Boletin07_1/src/main/java/com/mycompany/boletin07_1/NumeroPositivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.boletin07_1;
+
+/**
+ *
+ * @author dam1
+ */
+public class NumeroPositivo {
+    public void mostrarPositivo(int num1){
+        if(num1>0)
+            System.out.println(num1 + "É positivo");
+        
+    }
+    
+}
